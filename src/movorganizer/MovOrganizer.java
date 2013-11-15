7a55f2652d2b7840a9283c531de25a4b586436b6
Hexe <Hexe@192.168.2.9>
2013-11-15 08:24:30 -0600

    Inicio del proyecto

---
+++ src/movorganizer/MovOrganizer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movorganizer;
+
+/**
+ *
+ * @author Hexe
+ */
+public class MovOrganizer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
